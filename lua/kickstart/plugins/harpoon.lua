4e78739e04b0c2eb342f6fb11272a331eac7a52c
liconl <licon.luisangel@gmail.com>
2024-06-24 12:06:32 -0500

    tech: remap harpoon list

--- lua/kickstart/plugins/harpoon.lua
+++ lua/kickstart/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end, { desc = '[a]dd to Harpoon' })
-    vim.keymap.set('n', '<C-h>', function()
+    vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = '[H]arpoon Menu' })
     vim.keymap.set('n', '<C-j>', function()
