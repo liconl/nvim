6b98e726f84caf75f1315300545cab9908a475be
liconl <licon.luisangel@gmail.com>
2024-07-08 15:34:28 -0500

    tech: start carbon fox not tokyonight

--- lua/kickstart/plugins/nightfox.lua
+++ lua/kickstart/plugins/nightfox.lua
@@ -1,8 +1,13 @@
 -- nightfox
 -- https://github.com/EdenEast/nightfox.nvim
-
+--
 return {
-  'EdenEast/nightfox.nvim',
-  config = function() end,
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'carbonfox'
+    end,
+  },
 }
-
+-- vim: ts=2 sts=2 sw=2 et
