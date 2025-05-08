237763639438200220d53cb03758c251e67b9eba
liconl <licon.luisangel@gmail.com>
2025-05-07 20:50:31 -0500

    tech: persist preview when buffer is removed

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,14 @@
 -- https://github.com/iamcco/markdown-preview.nvim
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && npm install',
   init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_filetypes = { 'markdown' }
+    vim.g.mkdp_auto_close = 0
   end,
-  ft = { "markdown" },
+  ft = { 'markdown' },
+  keys = {
+    { '<leader>m', '<cmd>MarkdownPreview<CR>', desc = 'Preview Markdown' },
+  },
 }
